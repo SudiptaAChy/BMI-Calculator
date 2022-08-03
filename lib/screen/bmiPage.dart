a4feb71b460041dea41f651a15f59dfb8362fa58
Peal Mazumder <peal6614@gmail.com>
2022-08-03 18:52:40 +0600

    adds BMI UI

--- lib/screen/bmiPage.dart
+++ lib/screen/bmiPage.dart
@@ -10,12 +10,63 @@ class BmiPage extends StatefulWidget {
 }
 
 class _BmiPageState extends State<BmiPage> {
+  TextEditingController weightController = new TextEditingController();
+  TextEditingController heightController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: const Text("Hello"),
+        body: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/logo.png',
+                height: 200,
+                width: 400,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: weightController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Weight(kg)',
+                  hintText: 'Enter Your Weight',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: heightController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Height(cm)',
+                  hintText: 'Enter Your Height',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: calculatePress,
+                child: Text('Calculate'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  void calculatePress() {}
 }
